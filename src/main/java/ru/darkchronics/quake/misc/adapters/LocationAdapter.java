8b473972f6550a6fd522bf87af16cca7f28729b1
polyzium <polyzium@disroot.org>
2025-05-15 19:45:56 +0300

    Add licenses

--- src/main/java/ru/darkchronics/quake/misc/adapters/LocationAdapter.java
+++ src/main/java/ru/darkchronics/quake/misc/adapters/LocationAdapter.java
@@ -1,3 +1,22 @@
+/*
+ * DarkChronics-Quake, a Quake minigame plugin for Minecraft servers running PaperMC
+ * 
+ * Copyright (C) 2024-present Polyzium
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package ru.darkchronics.quake.misc.adapters;
 
 import com.google.gson.*;
